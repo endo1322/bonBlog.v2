0ab1a0a9761572444c2c0f50ca20fd3b9f2a7f54
bondroid1322 <bondroid3.22@icloud.com>
2025-02-27 21:01:21 +0900

    add: clientからserverのAPIを叩けるように実装

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createClient } from "../../server/src/index";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  const client = createClient("http://localhost:8787");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await client.index.$get({
+        json: {
+          name: "John Doe",
+          age: 30,
+        },
+      });
+      console.log(res);
+      if (res.ok) {
+        const data = await res.json();
+        setMessage(data.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -18,9 +39,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>{message}</div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from "hono";
+import { hc } from "hono/client";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+const router = app
+  .use("/", cors({ origin: "http://localhost:5173" }))
+  .get("/", (c) => {
+    return c.json({ message: "Hello Hono!" });
+  });
 
 export default app;
+
+export type AppRouteType = typeof router;
+
+type ClientType = typeof hc<AppRouteType>;
+
+export const createClient = (
+  ...args: Parameters<ClientType>
+): ReturnType<ClientType> => {
+  return hc<AppRouteType>(...args);
+};
