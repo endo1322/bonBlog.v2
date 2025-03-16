af81cdf96a0883e08ebac37ef974a7b0aa0de33e
bondroid1322 <bondroid3.22@icloud.com>
2025-03-16 18:15:36 +0900

    chore: formatter

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { createClient } from "../../server/src/index";
 
@@ -21,15 +21,15 @@ function App() {
     };
 
     fetchData();
-  }, []);
+  }, [client]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -40,9 +40,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more!
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more!</p>
     </>
   );
 }
