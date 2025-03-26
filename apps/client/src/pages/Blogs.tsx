0d6f6495367550c496dd0a5f231690cab94d7e04
bondroid1322 <bondroid3.22@icloud.com>
2025-03-26 18:00:39 +0900

    chore: blogsページでブログ一覧を表示するように変更

--- apps/client/src/pages/Blogs.tsx
+++ apps/client/src/pages/Blogs.tsx
@@ -1,5 +1,31 @@
+import rpcClient, { type BogsResponseType } from "@/apis";
+import { useEffect, useState } from "react";
+
 const Blogs = () => {
-  return <div>Blogs</div>;
+  const [blogs, setBlogs] = useState<BogsResponseType>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await rpcClient.blogs.$get();
+      if (res.ok) {
+        const data = await res.json();
+        setBlogs(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Blogs Page</h1>
+      <div>
+        {blogs.map((blog) => (
+          <div key={blog.id}>{blog.title}</div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Blogs;
