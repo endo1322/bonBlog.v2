87281933e6ab7f93d3737799b7bf304fb19601a8
bondroid1322 <bondroid3.22@icloud.com>
2025-03-28 13:26:34 +0900

    chore: tailwindcss install

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <div>{message}</div>
         <p>
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -5,5 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
