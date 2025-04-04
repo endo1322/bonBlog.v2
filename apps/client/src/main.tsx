54b1241c9dea30c0571b883b115c5b032bbc63d3
bondroid1322 <bondroid3.22@icloud.com>
2025-04-04 22:56:22 +0900

    delete: 不要なindex.cssの削除

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,6 @@
+import router from "@/routes.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import router from "@/routes.tsx";
 import { RouterProvider } from "react-router";
 import "@bonblogv2/ui/globals.css";
 
