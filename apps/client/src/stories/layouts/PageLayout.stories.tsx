244aee2bbb5e7ef9eede9f71ea9bfb19d3d04218
bondroid1322 <bondroid3.22@icloud.com>
2025-04-05 00:37:22 +0900

    add: PageLayout系のstoryの作成

---
+++ apps/client/src/stories/layouts/PageLayout.stories.tsx
@@ -0,0 +1,36 @@
+import { PageLayout } from "@/components/layouts";
+import type { Meta, StoryObj } from "@storybook/react";
+import { withRouter } from "storybook-addon-remix-react-router";
+
+const meta = {
+  title: "Layouts/PageLayout",
+  component: PageLayout,
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    title: {
+      control: { type: "text" },
+      description: "The title of the page",
+      table: {
+        type: { summary: "string" },
+      },
+    },
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  decorators: [withRouter],
+} satisfies Meta<typeof PageLayout>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: "Page",
+    children: <div>Page Content</div>,
+  },
+};
