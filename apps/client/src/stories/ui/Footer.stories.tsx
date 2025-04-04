e051387b3ddef008ff87470ecc2e40a1ae5c677c
bondroid1322 <bondroid3.22@icloud.com>
2025-04-04 22:34:21 +0900

    add: layout類のstoryを作成

---
+++ apps/client/src/stories/ui/Footer.stories.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/ui/Footer";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "UI/Footer",
+  component: Footer,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ apps/client/src/stories/ui/Header.stories.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/ui/Header";
+import type { Meta, StoryObj } from "@storybook/react";
+import { withRouter } from "storybook-addon-remix-react-router";
+
+const meta = {
+  title: "UI/Header",
+  component: Header,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [withRouter],
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
