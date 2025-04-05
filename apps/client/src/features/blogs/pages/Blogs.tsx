7e05a47910d5968a06a28f3a7bd12046e886ec3f
bondroid1322 <bondroid3.22@icloud.com>
2025-04-05 22:35:08 +0900

    change: Suspenseで読み込み中にスケルトン表示になるように変更

--- apps/client/src/features/blogs/pages/Blogs.tsx
+++ apps/client/src/features/blogs/pages/Blogs.tsx
@@ -1,26 +1,28 @@
-import rpcClient, { type BlogsResponseType } from "@/apis";
+import rpcClient from "@/apis";
 import { PageLayout } from "@/components/layouts";
 import { BlogCardList } from "@/features/blogs/components/BlogCardList";
-import { useEffect, useState } from "react";
+import { BlogCardListSkeleton } from "@/features/blogs/components/BlogCardListSkeleton";
+import type { Blogs } from "@/features/blogs/types/blog";
+import { Suspense, use, useState } from "react";
+
+const Child: React.FC<{ blogsP: Promise<Blogs> }> = ({ blogsP }) => {
+  const blogs = use(blogsP);
+  return <BlogCardList blogs={blogs} />;
+};
 
 const BlogsPage = () => {
-  const [blogs, setBlogs] = useState<BlogsResponseType>([]);
+  const fetchData = async () => {
+    const res = await rpcClient.blogs.$get();
+    return await res.json();
+  };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await rpcClient.blogs.$get();
-      if (res.ok) {
-        const data = await res.json();
-        setBlogs(data);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const [blogs] = useState(() => fetchData());
 
   return (
     <PageLayout title={"Blog"}>
-      <BlogCardList blogs={blogs} />
+      <Suspense fallback={<BlogCardListSkeleton />}>
+        <Child blogsP={blogs} />
+      </Suspense>
     </PageLayout>
   );
 };
