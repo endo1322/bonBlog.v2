3ffd4ec5ed4f03564dd1153f54603e48e88ef0e6
bondroid1322 <bondroid3.22@icloud.com>
2025-04-05 20:54:15 +0900

    add: BlogCardListのスケルトンUIの作成

---
+++ apps/client/src/features/blogs/components/BlogCardListSkeleton.tsx
@@ -0,0 +1,48 @@
+import { Card } from "@/components/ui";
+import { Skeleton } from "@bonblogv2/ui/components";
+import { cn } from "@bonblogv2/ui/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+const TimestampSkeleton: React.FC<Props> = ({ className }) => {
+  return <Skeleton className={cn("h-5 w-20", className)} />;
+};
+
+const BlogTitleSkeleton: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={cn("flex flex-col gap-2", className)}>
+      <Skeleton className="h-6" />
+      <Skeleton className="h-6" />
+    </div>
+  );
+};
+
+const TagSkeleton: React.FC = () => {
+  return <Skeleton className="h-5 w-16 rounded-full" />;
+};
+
+const BlogCardSkeleton: React.FC = () => {
+  return (
+    <Card>
+      <TimestampSkeleton className="mb-2" />
+      <BlogTitleSkeleton className="mb-4" />
+      <div className="flex flex-wrap gap-2">
+        {[1, 2, 3].map((i) => (
+          <TagSkeleton key={i} />
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+export const BlogCardListSkeleton: React.FC = () => {
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {[...Array(12)].map((i) => (
+        <BlogCardSkeleton key={i} />
+      ))}
+    </div>
+  );
+};
