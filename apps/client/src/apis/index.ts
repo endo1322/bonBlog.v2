6e124f8486d1b047f64f3de39f910713969a99e1
bondroid1322 <bondroid3.22@icloud.com>
2025-04-05 15:54:32 +0900

    add: blogs pageのstoryとblogsエンドポイントのmsw handlerを作成

--- apps/client/src/apis/index.ts
+++ apps/client/src/apis/index.ts
@@ -1,6 +1,6 @@
+import { serverUrl } from "@/constants/env";
 import { type InferHonoType, createHonoClient } from "@bonblogv2/server/src";
 
-const serverUrl = import.meta.env.VITE_SERVER_URL;
 export const rpcClient = createHonoClient(serverUrl);
 
 export type BlogsResponseType = InferHonoType<typeof rpcClient.blogs.$get>;
