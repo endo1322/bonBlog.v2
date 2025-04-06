a783514c9fc52bc2477bb4aa506092144dfce5e2
bondroid1322 <bondroid3.22@icloud.com>
2025-04-06 17:26:57 +0900

    change: queryKeyをファイルで管理するように変更

--- apps/client/src/features/blogs/queries/blogList.ts
+++ apps/client/src/features/blogs/queries/blogList.ts
@@ -1,8 +1,9 @@
+import { QUERY_KEY } from "@/constants/queryKey";
 import { getBlogs } from "@/features/blogs/apis/blogs";
 import { queryOptions } from "@tanstack/react-query";
 
 export const blogListQuery = () =>
   queryOptions({
-    queryKey: ["blog", "list"],
+    queryKey: QUERY_KEY.BLOGLIST,
     queryFn: () => getBlogs(),
   });
