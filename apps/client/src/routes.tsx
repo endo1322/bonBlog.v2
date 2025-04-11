510ca28c727afe242731e431ee1002b4343ad56e
bondroid1322 <bondroid3.22@icloud.com>
2025-04-11 22:03:43 +0900

    add: クエリ確認用に簡易なブログページを作成

--- apps/client/src/routes.tsx
+++ apps/client/src/routes.tsx
@@ -1,6 +1,7 @@
 import App from "@/App";
 import { RootLayout } from "@/components/layouts";
 import { PATH } from "@/constants/path";
+import BlogDetailPage from "@/features/blogs/pages/BlogDetail";
 import BlogsPage from "@/features/blogs/pages/Blogs";
 import { createBrowserRouter } from "react-router";
 
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, Component: App },
       { path: PATH.BLOGS, Component: BlogsPage },
+      { path: PATH.BLOG_DETAIL, Component: BlogDetailPage },
     ],
   },
 ]);
