e83087d8d05c09081fb9c88026e618503cd71028
bondroid1322 <bondroid3.22@icloud.com>
2025-04-12 17:23:15 +0900

    fix: storyでreact-routerが反映されるように修正

--- apps/client/src/stories/features/blogs/ui/BlogListContainer.stories.tsx
+++ apps/client/src/stories/features/blogs/ui/BlogListContainer.stories.tsx
@@ -1,6 +1,7 @@
 import { BlogListContainer } from "@/features/blogs/components";
 import blogsHandlers from "@/mocks/handlers/blogs";
 import type { Meta, StoryObj } from "@storybook/react";
+import { withRouter } from "storybook-addon-remix-react-router";
 
 const meta = {
   title: "Features/Blogs/UI/BlogListContainer",
@@ -10,6 +11,7 @@ const meta = {
       handlers: blogsHandlers,
     },
   },
+  decorators: [withRouter],
 } satisfies Meta<typeof BlogListContainer>;
 
 export default meta;
