ee4d275662d7fc6401a8203389624c4dcc7e8bde
bondroid1322 <bondroid3.22@icloud.com>
2025-04-12 19:09:03 +0900

    change: 改行一つの場合、段落になるよう変更

---
+++ apps/server/src/utils/markdown.ts
@@ -0,0 +1,6 @@
+/**
+ * Notionで書かれたblogに一部適切でないmarkdownが含まれることがあるため整形を行う
+ */
+export const markdownFormatter = (markdown: string) => {
+  return markdown.replace(/・/g, "- ").replace(/([^\n-])\n([^\n-])/g, "$1\n\n$2");
+};
