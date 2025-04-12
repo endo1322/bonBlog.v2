6a3e36ecff5d5dc518079763fcae3d329f86ff7f
bondroid1322 <bondroid3.22@icloud.com>
2025-04-12 19:36:41 +0900

    chore: 不要な変換の考慮を削除

--- apps/server/src/utils/markdown.ts
+++ apps/server/src/utils/markdown.ts
@@ -2,5 +2,5 @@
  * Notionで書かれたblogに一部適切でないmarkdownが含まれることがあるため整形を行う
  */
 export const markdownFormatter = (markdown: string) => {
-  return markdown.replace(/・/g, "- ").replace(/([^\n-])\n([^\n-])/g, "$1\n\n$2");
+  return markdown.replace(/・/g, "- ").replace(/([^\n])\n([^\n-])/g, "$1\n\n$2");
 };
