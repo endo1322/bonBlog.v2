84709d1ccc6557a3c93dc311eeb83800b10a8592
bondroid1322 <bondroid3.22@icloud.com>
2025-04-21 22:32:34 +0900

    change: 400系のエラーは再フェッチしないように変更

--- apps/client/src/queries/index.ts
+++ apps/client/src/queries/index.ts
@@ -1,11 +1,21 @@
 import { getIndex } from "@/apis";
+import { HttpError } from "@/apis/HttpError";
 import { QUERY_KEY } from "@/constants/queryKey";
 import { QueryClient, queryOptions } from "@tanstack/react-query";
+
+const RETRY_COUNT = 3;
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
+      retry: (failureCount, error) => {
+        if (failureCount >= RETRY_COUNT) return false;
+        if (error instanceof HttpError) {
+          if (error.status >= 400 && error.status < 500) return false;
+        }
+        return true;
+      },
     },
   },
 });
