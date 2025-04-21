a06f7fd0008f2c5038bf84a25dc09fca9f776f08
bondroid1322 <bondroid3.22@icloud.com>
2025-04-21 22:34:50 +0900

    add: 共通のエラーフォーマットで返却するように実装

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,11 +1,18 @@
 import corsMiddleware from "@server/middlewares/cors";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import blogs from "./blogs";
 
 export const app = new Hono();
 
 const router = app
   .use("*", corsMiddleware)
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return c.json({ message: err.message }, err.status);
+    }
+    return c.json({ message: "Internal Server Error" }, 500);
+  })
   .get("/", (c) => c.json({ message: "Hello Hono!" }))
   .route("/blogs", blogs);
 
