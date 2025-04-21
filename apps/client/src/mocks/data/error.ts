c1c863313a951b2c0a54a09e7338ac7362a43c3e
bondroid1322 <bondroid3.22@icloud.com>
2025-04-21 22:33:07 +0900

    add: 404系のテストケースを追加

--- apps/client/src/mocks/data/error.ts
+++ apps/client/src/mocks/data/error.ts
@@ -1,3 +1,5 @@
 import type { ErrorResponseType } from "@/apis";
 
 export const mockErrorResponseData: ErrorResponseType = { message: "Internal Server Error" };
+
+export const mock404ErrorResponseData: ErrorResponseType = { message: "Not Found" };
--- apps/client/src/mocks/handlers/blogs.ts
+++ apps/client/src/mocks/handlers/blogs.ts
@@ -1,6 +1,6 @@
 import { serverUrl } from "@/constants/env";
 import { mockGetBlog200ResponseData, mockGetBlogs200ResponseData } from "@/mocks/data/blogs";
-import { mockErrorResponseData } from "@/mocks/data/error";
+import { mock404ErrorResponseData, mockErrorResponseData } from "@/mocks/data/error";
 import { http, HttpResponse } from "msw";
 
 const mockGetBlogs200SuccessHandler = http.get(`${serverUrl}/blogs`, () =>
@@ -13,6 +13,10 @@ const mockGetBlog200SuccessHandler = http.get(`${serverUrl}/blogs/:id`, () =>
 
 export const mockGetBlogs500ErrorHandler = http.get(`${serverUrl}/blogs`, () =>
   HttpResponse.json(mockErrorResponseData, { status: 500 }),
+);
+
+export const mockGetBlog404ErrorHandler = http.get(`${serverUrl}/blogs/:id`, () =>
+  HttpResponse.json(mock404ErrorResponseData, { status: 404 }),
 );
 
 export const mockGetBlog500ErrorHandler = http.get(`${serverUrl}/blogs/:id`, () =>
