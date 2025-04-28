00b13d86baf5e5848f67be50d1cc4b595a4508c6
bondroid1322 <bondroid3.22@icloud.com>
2025-04-28 19:43:47 +0900

    change: タイトルにサイト名を表示するように変更

--- apps/client/src/components/meta/Meta.tsx
+++ apps/client/src/components/meta/Meta.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const Meta = ({ title, auther, keywords }: Props) => {
   return (
     <>
-      <title>{title}</title>
+      <title>{`${title} | bonBlog.v2`}</title>
       {auther && <meta name="author" content={auther} />}
       {keywords && <meta name="keywords" content={keywords} />}
     </>
