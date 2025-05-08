5aa4ad550a758cc216c3767679871b100136894d
bondroid1322 <bondroid3.22@icloud.com>
2025-05-09 01:20:44 +0900

    add: blog routeでcacheMiddlewareを使用するように変更

--- apps/server/src/routes/blogs.ts
+++ apps/server/src/routes/blogs.ts
@@ -1,12 +1,12 @@
 import { NotFoundError } from "@server/domain/errors/NotFoundError";
-import BlogDIContainer from "@server/middlewares/blogDIContainer";
-import notionMiddleware from "@server/middlewares/notion";
+import { blogDIContainer, cacheMiddleware, notionMiddleware } from "@server/middlewares";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 const blogs = new Hono()
   .use("*", notionMiddleware)
-  .use("*", BlogDIContainer)
+  .use("*", blogDIContainer)
+  .use("*", cacheMiddleware)
   .get("/", async (c) => {
     const blogUseCase = c.get("blogUseCase");
     const result = await blogUseCase.getAllBlogs();
